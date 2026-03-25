41a43c75da09eac55ecb6235496b2c657e388e57
Hamnah Qureshi <hamnahq25@gmail.com>
2026-03-25 20:44:53 +0000

    feat: Add logic to ensure params never out of range and show up grey in sim

--- model/cells/nectar_cell.hpp
+++ model/cells/nectar_cell.hpp
@@ -36,22 +36,25 @@ public:
         // Carrying capacity per cell
         double max_nectar = 100.0;
         double max_pollen = 50.0;
-        //TODO: max num of bees
+        double max_bees = 50.0;
 
         int incoming_bees = 0;
         for (const auto& [neighborId, neighborData] : neighborhood) {
+            if (neighborId[0] == 0 && neighborId[1] == 0) continue;
             incoming_bees += static_cast<int>(neighborData.state->bees * 0.25);
         }
         // Keep half of local bees and add incoming bees
         int new_bees = static_cast<int>(incoming_bees) + (state.bees / 2);
 
-
-        
+        // Keep num of bees in range
         newState.bees = new_bees;
+        if (newState.bees < 0) newState.bees = 0;
+        if (newState.bees > max_bees) newState.bees = max_bees;
 
         // Nectar dynamics
-        if (state.nectar_lvl < max_nectar) {
-            newState.nectar_lvl += nectar_regrowth;
+        double nectar_after_growth = state.nectar_lvl + nectar_regrowth;
+        if (nectar_after_growth > max_nectar) {
+            nectar_after_growth = max_nectar;
         }
         newState.nectar_lvl -= nectar_decay * state.nectar_lvl;
         newState.nectar_lvl -= nectar_consumption * newState.bees;
