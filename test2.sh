0f0e6aee5d25592584ee1dfa9e9505af3d8f0536
Hamnah Qureshi <hamnahq25@gmail.com>
2026-05-06 01:20:48 +0000

    fix: test2 script

--- test2.sh
+++ test2.sh
@@ -4,4 +4,4 @@
 # Output log:    simulation_results/test2_grid_log.csv
 mkdir -p simulation_results
 ./bin/nectar config/tests/test2_center_burst_config.json simulation_results/test2_grid_log.csv
-awk -F';' 'NR<=2 || ($3 ~ /^\([0-9]+,[0-9]+\)$/ && $5 ~ /^<[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+>$/) { print }' "simulation_results/grid_log.csv" > "simulation_results/grid_log.csv.tmp" && mv -f "simulation_results/grid_log.csv.tmp" "simulation_results/grid_log.csv"
+awk -F';' 'NR<=2 || ($3 ~ /^\([0-9]+,[0-9]+\)$/ && $5 ~ /^<[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+,[^,]+>$/) { print }' "simulation_results/test2_grid_log.csv" > "simulation_results/test2_grid_log.csv.tmp" && mv -f "simulation_results/test2_grid_log.csv.tmp" "simulation_results/test2_grid_log.csv"
